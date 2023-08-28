50258099803f2c200ac28a795a94e6618aa128cc
Hamza Khalid <hamzakhalid829@gmail.com>
2023-08-28 22:26:34 +0500

    DB Added

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
+import logging
+import sqlite3
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder
 from twscrape import API, gather
 from twscrape.logger import set_log_level
 from telegram.helpers import escape_markdown
-import time
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
 
 load_dotenv()
 
@@ -12,40 +17,47 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
 TWITTER_USERNAME = os.getenv("TWITTER_USERNAME")
 api = API("accounts.db") 
-app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-job_queue = app.job_queue
+
+# SQLite setup
+conn = sqlite3.connect('tweets.db')
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE IF NOT EXISTS posted_tweets 
+    (tweet_id TEXT PRIMARY KEY)
+''')
+conn.commit()
+
+def is_tweet_posted(tweet_id):
+    c.execute("SELECT tweet_id FROM posted_tweets WHERE tweet_id=?", (tweet_id,))
+    return c.fetchone() is not None
+
+def mark_tweet_as_posted(tweet_id):
+    c.execute("INSERT INTO posted_tweets (tweet_id) VALUES (?)", (tweet_id,))
+    conn.commit()
 
 async def post_to_telegram(app):
     global api
-    print(time.strftime("%H:%M:%S", time.localtime()), "Checking for new tweets...")
 
     tweet = (await gather(api.user_tweets(TWITTER_USERNAME, limit=1)))[1]
-    print(tweet.rawContent)
-    # Check if it's not a retweet or reply
-    if (tweet.retweetedTweet is None and tweet.inReplyToTweetId is None and tweet.inReplyToUser is None and tweet.quotedTweet is None):
-        print(time.strftime("%H:%M:%S", time.localtime()), "New tweet found, posting to Telegram...")
-        
-        # Create the pretty message
-        content = tweet.rawContent
-        # Split the content and remove the last word
-        content = content.split(" ")
+
+    if is_tweet_posted(tweet.id_str):
+        return
+
+    if (tweet.retweetedTweet is None and tweet.inReplyToTweetId is None 
+        and tweet.inReplyToUser is None and tweet.quotedTweet is None):
+
+        content = tweet.rawContent.split(" ")
         tweet_link = content.pop()
-        # Join the content back together
         content = " ".join(content)
-
-        # Escape only the content part
         escaped_content = escape_markdown(content, version=1)
-
         msg = f"{escaped_content}\n\n🔗 [Twitter Link]({tweet_link})"
 
-        print(time.strftime("%H:%M:%S", time.localtime()), "Tweet content:\n", msg, "\n\n")
         try:
-            # Handle media
             if tweet.media:
                 if tweet.media.photos:
                     for photo in tweet.media.photos:
                         await app.bot.send_photo(chat_id=TELEGRAM_CHANNEL_ID, photo=photo.url, caption=msg)
-                        msg = None  # Ensure caption is only added once
+                        msg = None
                 elif tweet.media.videos:
                     video = tweet.media.videos[0].variants[0] if tweet.media.videos else None
                     if video:
@@ -57,15 +69,20 @@ async def post_to_telegram(app):
                         await app.bot.send_animation(chat_id=TELEGRAM_CHANNEL_ID, animation=animated.videoUrl, caption=msg)
                         msg = None
 
-            # Send text if there was no media or if there was additional text content
             if msg:
                 await app.bot.send_message(chat_id=TELEGRAM_CHANNEL_ID, text=msg, parse_mode="Markdown")
+
+            mark_tweet_as_posted(tweet.id_str)
+
         except Exception as e:
-            print(f"Error while posting to Telegram: {e}")
-    else:
-        print("Tweet is a reply or retweet, ignoring.")
+            logger.error(f"Error while posting to Telegram: {e}")
 
-set_log_level("DEBUG")  # set log level to debug to see more info
-job_queue.run_repeating(post_to_telegram, interval=60)
-app.run_polling()
+def main():
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    job_queue = app.job_queue
+    set_log_level("ERROR")  # set log level to error to minimize logging from twscrape
+    job_queue.run_repeating(post_to_telegram, interval=60)
+    app.run_polling()
 
+if __name__ == "__main__":
+    main()
